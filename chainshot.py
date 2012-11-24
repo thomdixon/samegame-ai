87312190b9a231f954346bf19d9ed7f1d96079ab
Thom Dixon <thom@thomdixon.org>
2012-11-24 02:02:01 -0500

    Implemented best-first search and parallel best-first search.
    Implemented best-first search using the heuristic of ``least
    number of tiles remaining on the board.'' Implemented a parallel
    variant of best-first search, where the board is split into four
    (not necessarily disjoint) subsquares of equal size (intuitively
    representing the NW, NE, SW and SE portions of the board), and
    then best-first search is applied to each individual subsquare
    in a separate process.

--- chainshot.py
+++ chainshot.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python
 
 import os.path
+import operator
+import time
+from multiprocessing import Pool
 
 class SameGameBoard(list):
+    # TODO: Print row and column numbers
     def __str__(self):
         r = ''
         for i in self:
             r += ''.join([str(j) if j else '*' for j in i])+'\n'
         return r[:-1]
 
+    def size(self):
+        if not hasattr(self, '_size'):
+            self._size = len(self)**2
+
+        return self._size
+
+    def copy(self):
+        return SameGameBoard([list(i) for i in self])
+
     def occupied(self):
-        return len(self)**2 - sum(i.count(None) for i in self)
+        return self.size() - sum(i.count(None) for i in self)
 
     def transpose(self):
         return SameGameBoard([[self[i][j] for i in xrange(len(self))] for j in xrange(len(self))])
@@ -49,28 +62,82 @@ class SameGameBoard(list):
         return count
 
     def chain_length(self, row, column):
-        return -1 if self[row][column] is None else self._chain_length(row, column, SameGameBoard([list(i) for i in self]))
+        return -1 if self[row][column] is None else self._chain_length(row, column, self.copy())
 
     def remove_chain(self, row, column):
         return -1 if self[row][column] is None else self._chain_length(row, column, self)
 
     def end_game(self):
         for i in xrange(len(self)-1, 0, -1):
-            for j in xrange(len(self)-1, 0, -1):
+            for j in xrange(len(self)):
                 if self.chain_length(i, j) > 2:
                     return False
         return True
 
-    def translate_coordinates(self, x, y):
+    def translate_coords(self, x, y):
         return (len(self) - x, y - 1)
+
+    def inverse_translate_coords(self, x, y):
+        return (len(self) - x, y + 1)
+
+# NOTE: These don't belong to a class because Python has issues
+#       serializing instance methods of classes for use with the
+#       multiprocessing module.
+def _best_first_search_core(board):
+    '''Return the best move for the supplied board.'''
+    working_board = board.copy()
+    queue = []
+    for i in xrange(len(working_board)-1, 0, -1):
+        for j in xrange(len(working_board)-1, 0, -1):
+            removed = working_board.chain_length(i, j)
+            if removed > 2:
+                queue.append(((i, j), removed))
+                working_board.remove_chain(i, j)
+
+    if not queue:
+        return None
+    return max(queue, key=operator.itemgetter(1))
+
+def _parallel_best_first_search_core(board, pool):
+    '''Splits the board into four segments and performs best-first search on each.'''
+    even = len(board) % 2 == 0
+    size = (len(board)/2) if even else (len(board)/2 + 1)
+    boards = []
+
+    boards.append(SameGameBoard([i[:size] for i in board[:size]]))
+    boards.append(SameGameBoard([i[(size if even else size-1):] for i in board[:size]]))
+    boards.append(SameGameBoard([i[:size] for i in board[(size if even else size-1):]]))
+    boards.append(SameGameBoard([i[(size if even else size-1):] for i in board[(size if even else size-1):]]))
+
+    processed = pool.map(_best_first_search_core, boards)
+    results = []
+
+    # Adjust the coordinates for the larger board
+    if processed[0] is not None:
+        results.append(processed[0])
+    if processed[1] is not None:
+        results.append(((processed[1][0][0], processed[1][0][1]+(size if even else size-1)), processed[1][1]))
+    if processed[2] is not None:
+        results.append(((processed[2][0][0]+(size if even else size-1), processed[2][0][1]), processed[2][1]))
+    if processed[3] is not None:
+        results.append(((processed[3][0][0]+(size if even else size-1), processed[3][0][1]+(size if even else size-1)), processed[3][1]))
+
+    if not results:
+        return _best_first_search_core(board)
+    return max(results, key=operator.itemgetter(1))
+
+def _parallel_best_n_search_core():
+        pass
 
 class SameGame(object):
     def print_main_menu(self):
         print "Welcome to Chainshot!"
         print "====================="
         print "1) Play"
-        print "2) Rules"
-        print "3) Exit"
+        print "2) Best-first search"
+        print "3) Parallel best-first search"
+        print "4) Rules"
+        print "5) Exit"
 
     def print_rules(self):
         print
@@ -94,7 +161,7 @@ should be entered as: x y. For example, 5 2 corresponds to
         print
         try:
             choice = int(raw_input('What would you like to do? '))
-            if 0 < choice and choice < 4:
+            if 0 < choice and choice < 6 :
                 return choice
         except:
             pass
@@ -111,7 +178,7 @@ should be entered as: x y. For example, 5 2 corresponds to
                 if i < 0 or i > len(self.board):
                     raise
 
-            if self.board.chain_length(*self.board.translate_coordinates(*move)) < 3:
+            if self.board.chain_length(*self.board.translate_coords(*move)) < 3:
                 raise
             
             return move
@@ -119,14 +186,46 @@ should be entered as: x y. For example, 5 2 corresponds to
             print 'This move is invalid. Please try again.'
 
         return self.get_move()
-     
+
+    def best_first_search(self, board_path, alg):
+        '''Best-first search using the heuristic of most tiles removed.'''
+        self.board = SameGameBoard([list(i.strip()) for i in open(board_path, 'r').readlines()])
+        print self.board
+        score = 0
+        moves = []
+
+        if alg == _parallel_best_first_search_core:
+            alg = lambda x: _parallel_best_first_search_core(x, Pool(4))
+
+        start = time.clock()
+        while not self.board.end_game():     
+            best = alg(self.board)
+            moves.append(best[0])
+            score += (self.board.remove_chain(*best[0])-2)**2
+            self.board = self.board.collapse()
+            print
+            print self.board
+        elapsed = time.clock() - start
+
+        print
+        unoccupied = self.board.size() - self.board.occupied()
+        percentage = int(float(unoccupied)/self.board.size()*100)
+        print 'Game over! Took %.3f seconds.' % elapsed
+        print 'Cleared %d/%d tiles (%d%%) in %d moves for a score of %d.' % (unoccupied,
+                                                                             self.board.size(),
+                                                                             percentage,
+                                                                             len(moves),
+                                                                             score)
+        print [self.board.inverse_translate_coords(*i) for i in moves]
+        print
+
     def play_game(self, board_path):
         self.board = SameGameBoard([list(i.strip()) for i in open(board_path, 'r').readlines()])
         print self.board
         score = 0
         while not self.board.end_game():
             move = self.get_move()
-            result = (self.board.remove_chain(*self.board.translate_coordinates(*move)) - 2)**2
+            result = (self.board.remove_chain(*self.board.translate_coords(*move)) - 2)**2
             print 'Your move (%d, %d) increased your score by %d' % (move[0], move[1], result)
             score += result
             print 'Score: %d' % score
@@ -143,8 +242,7 @@ should be entered as: x y. For example, 5 2 corresponds to
             return board
         else:
             print "I'm sorry, but that path was invalid."
-            return self.get_board()
-            
+            return self.get_board()         
 
     def chainshot(self):
         while True:
@@ -153,6 +251,10 @@ should be entered as: x y. For example, 5 2 corresponds to
             if choice == 1:
                 self.play_game(self.get_board())
             elif choice == 2:
+                self.best_first_search(self.get_board(), _best_first_search_core)
+            elif choice == 3:
+                self.best_first_search(self.get_board(), _parallel_best_first_search_core)
+            elif choice == 4:
                 self.print_rules()
             else:
                 break
